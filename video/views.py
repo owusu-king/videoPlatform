6fe5c62158b4101e9eb85a3b0635dd59c08c137c
Owusu King <owusuking401@gmail.com>
2024-06-08 12:15:35 +0000

    Completed login view, its url and redirection to signup signin accordingly. also added a logout view

--- video/views.py
+++ video/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .forms import UserCreationForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Video page, denoting the index view
 def index(request): 
@@ -18,3 +22,24 @@ def signup_view(request):
         else:
             return render(request, 'video/signup.html', {'signup':form}) # if form has errors, then show the errors with the form again  
     return render(request, 'video/signup.html', {'signup':UserCreationForm()}) # if request method is GET, provide a new sign up form
+
+
+# Login View, for user login action
+def login_view(request):
+    if request.method == 'POST':
+        # if the request method is POST, get the form fields and authenticate the user
+        username = request.POST['username']
+        password = request.POST['password']        
+        user = authenticate(request, username=username, password = password) # authenticate with django's default authenticate module
+        if user is not None:
+            login(request, user)
+        else:
+            return render(request, 'video/login.html', {'message':"Invalid Credentials"}) # If authentication, fails, display login page again, with error message
+
+    return render(request, 'video/login.html') # Defaut request method is GET, so, provide a new login form
+
+
+# Logout view, destroy user session and take them to the login page
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
