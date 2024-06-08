5a6304ad1a1aa039337ca94f590fbbb0067ffafb
Owusu King <owusuking401@gmail.com>
2024-06-08 09:08:47 +0000

    Added styles to the signup form and processed the POST request

--- video/urls.py
+++ video/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.index, name='index'),
+    path('signup/', views.signup_view, name='signup'),
 
 ]
--- video/views.py
+++ video/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import UserCreationForm
 
 # Video page, denoting the index view
 def index(request): 
     return render(request, 'video/index.html',)
+
+
+
+# ACCOUNT CREATION, LOGIN AND LOGOUT VIEWS
+# Signup View, for user registration.
+def signup_view(request): 
+    if request.method == 'POST':
+        # when the request method of this view is a POST, then get the form object and save the fields to the db
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            return render(request, 'video/signup.html', {'signup':form}) # if form has errors, then show the errors with the form again  
+    return render(request, 'video/signup.html', {'signup':UserCreationForm()}) # if request method is GET, provide a new sign up form
