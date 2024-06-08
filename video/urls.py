7346da08590785b1986f5083cd8dae25cb9f7854
Owusu King <owusuking401@gmail.com>
2024-06-08 06:35:12 +0000

    Added a header layout, home view and url

--- video/urls.py
+++ video/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('home/', views.index, name='index'),
+
+]
--- video/views.py
+++ video/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Video page, denoting the index view
+def index(request): 
+    return render(request, 'video/index.html',)
