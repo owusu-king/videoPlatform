18b3994736f82b30511c2759fe18156fc764582a
Owusu King <owusuking401@gmail.com>
2024-06-16 16:04:22 +0000

    All requirements satisfied

--- video/models.py
+++ video/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Video(models.Model):
-    title = models.CharField(max_length=70)
+    title = models.CharField(max_length=50)
     description = models.TextField(max_length=150)
     video_file = models.FileField(upload_to='videos/')
     upload_date = models.DateTimeField(auto_now_add=True)
